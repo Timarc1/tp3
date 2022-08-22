73c555b9f0dc97d3253bd82955863af3f4e88d5c
Gabriel Fortier <gabriel_fortier1@hotmail.com>
2022-08-22 17:35:13 -0400

    Contact + css

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
