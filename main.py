d80db53d7b7f6e30d003ff8db0f87e3f8e36f6f6
Marc-Andre Trudel <marc.andre.trudel123@gmail.com>
2022-08-22 18:50:29 -0400

    Modification index.html et main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from ordinaland import *
 from flask import Flask, request, render_template
 app = Flask(__name__)
+from random import choice
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    article1 = choice(articles)
+    article2 = choice(articles)
+    article3 = choice(articles)
+    liste_articles = [article1,article2,article3]
+
+    return render_template("index.html",liste_articles=liste_articles)
 
 @app.route("/contact")
 def contact():
@@ -12,3 +18,4 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
