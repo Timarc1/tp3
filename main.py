b2a99e010974ca4a08834082ca6326b66325d148
Gabriel Fortier <gabriel_fortier1@hotmail.com>
2022-08-22 20:38:36 -0400

    update css+ 3 derniers articles

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from random import choice
 
 @app.route("/")
 def index():
-    article1 = choice(articles)
-    article2 = choice(articles)
-    article3 = choice(articles)
+    article1 = (articles[len(articles)-1])
+    article2 = (articles[len(articles)-2])
+    article3 = (articles[len(articles)-3])
     liste_articles = [article1,article2,article3]
 
     return render_template("index.html",liste_articles=liste_articles)
+
+@app.route("/construire-ordinateur")
+def construire():
+    return render_template("construire-ordinateur.html")
 
 @app.route("/contact")
 def contact():
