85058c11d71fea864455af44f962b0167cd0cd31
Gabriel Fortier <gabriel_fortier1@hotmail.com>
2022-08-23 09:28:32 -0400

    ajustement fichier

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import paginate
 from ordinaland import *
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -16,13 +18,21 @@ def index():
 def construire():
     return render_template("construire-ordinateur.html")
 
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
+
+@app.route("/blog/<n>")
+def blog(n):
+    nbr_pages = math.ceil(len(articles) / 3)
+    pages = paginate.Page(articles, n, 3)
+    long = len(pages)
+    return render_template("blog.html",pages=pages,long=long,nbr_pages=nbr_pages)
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/glossaire")
+def glossaire():
+    return render_template(("glossaire.html"))
 
 if __name__ == "__main__":
     app.run(debug=True)
